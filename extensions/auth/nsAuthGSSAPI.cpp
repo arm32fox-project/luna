cfbd714081238d39bce143babafd509b9e8469c7
Jan Beich <jbeich@tormail.org>
2013-11-19 15:42:55 +0100

    Bug 810716 - Detect res_ninit() on DragonFly and FreeBSD. r=ted

--- extensions/auth/nsAuthGSSAPI.cpp
+++ extensions/auth/nsAuthGSSAPI.cpp
@@ -39,6 +39,9 @@ typedef KLStatus (*KLCacheHasValidTickets_type)(
 #endif
 
 #if defined(HAVE_RES_NINIT)
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <arpa/nameser.h>
 #include <resolv.h>
 #endif
 
