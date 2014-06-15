a615e4e14856241828d690309f95957452d327a4
access2godzilla <access2godzilla@forum.palemoon.org>
2014-06-15 14:31:42 +0200

    Mobile branding, Part 1

--- mobile/android/branding/official/configure.sh
+++ mobile/android/branding/official/configure.sh
@@ -2,7 +2,7 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-MOZ_APP_DISPLAYNAME=Firefox
-ANDROID_PACKAGE_NAME=org.mozilla.firefox
+MOZ_APP_DISPLAYNAME="Pale Moon"
+ANDROID_PACKAGE_NAME=org.palemoon.moonchild
 MOZ_UPDATER=
 MOZ_ANDROID_ANR_REPORTER=
