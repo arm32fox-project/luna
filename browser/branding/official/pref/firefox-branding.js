12fa2a2ba0ea14fc536c64edd64afb3f4dfce4f0
Pale Moon <git-repo@palemoon.org>
2014-07-11 21:39:33 +0200

    Update site-specific UAs

--- browser/branding/official/pref/firefox-branding.js
+++ browser/branding/official/pref/firefox-branding.js
@@ -68,6 +68,8 @@ pref("browser.search.param.yahoo-fr-ja", "mozff");
 pref("general.useragent.override.live.com","Mozilla/5.0 (Windows NT 6.1; WOW64; rv:28.0) Gecko/20100101 Firefox/28.0 (Pale Moon)");
 pref("general.useragent.override.outlook.com","Mozilla/5.0 (Windows NT 6.1; WOW64; rv:28.0) Gecko/20100101 Firefox/28.0 (Pale Moon)");
 pref("general.useragent.override.web.de","Mozilla/5.0 (Windows NT 6.1; WOW64; rv:28.0) Gecko/20100101 Firefox/28.0 (Pale Moon)");
+pref("general.useragent.override.aol.com","Mozilla/5.0 (Windows NT 6.1; WOW64; rv:28.0) Gecko/20100101 Firefox/28.0 (Pale Moon)");
+pref("general.useragent.override.google.com","Mozilla/5.0 (Windows NT 6.1; WOW64; rv:24.7) Gecko/24 PaleMoon/99.0");
 
 // ****************** Extensions/plugins ******************
 
