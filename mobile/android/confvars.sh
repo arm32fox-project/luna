5a1508027455e5852cdfdb0585359c4b0a8ec2e5
Pale Moon <git-repo@palemoon.org>
2014-09-03 13:04:47 +0200

    Mobile version bump

--- mobile/android/confvars.sh
+++ mobile/android/confvars.sh
@@ -5,7 +5,7 @@
 MOZ_APP_BASENAME=Palemoon
 MOZ_APP_VENDOR=Moonchild
 
-MOZ_APP_VERSION=25.0b1
+MOZ_APP_VERSION=25.0b2
 MOZ_APP_UA_NAME=PaleMoon
 
 MOZ_BRANDING_DIRECTORY=mobile/android/branding/unofficial
