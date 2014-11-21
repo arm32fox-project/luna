750ec2a66bdaa36a3eb17b4250adcbe5fb13926b
Pale Moon <git-repo@palemoon.org>
2014-11-21 21:55:18 +0100

    Update UA overrides for Netflix and Facebook

--- browser/branding/official/pref/firefox-branding.js
+++ browser/branding/official/pref/firefox-branding.js
@@ -72,7 +72,8 @@ pref("general.useragent.override.web.de","Mozilla/5.0 (Windows NT 6.1; WOW64; rv
 pref("general.useragent.override.aol.com","Mozilla/5.0 (Windows NT 6.1; WOW64; rv:28.0) Gecko/20100101 Firefox/28.0 (Pale Moon)");
 
 // UA-Sniffing domains below are pending responses from their operators - temp workaround
-pref("general.useragent.override.netflix.com","Mozilla/5.0 (Windows NT 6.1; WOW64; rv:25.0) Gecko/20100101 Firefox/24.9 PaleMoon/25.0");
+pref("general.useragent.override.netflix.com","Mozilla/5.0 (Windows NT 6.1; WOW64; rv:25.1) Gecko/20100101 Firefox/24.9 PaleMoon/25.1.0");
+pref("general.useragent.override.facebook.com","Mozilla/5.0 (Windows NT 6.1; WOW64; rv:25.1) Gecko/20141110 PaleMoon/25.1.0");
 
 // UA-Sniffing domains below have indicated no interest in supporting Pale Moon (BOO!)
 pref("general.useragent.override.humblebundle.com","Mozilla/5.0 (Windows NT 6.1; WOW64; rv:33.0) Gecko/20100101 Firefox/33.0 (Pale Moon)");
