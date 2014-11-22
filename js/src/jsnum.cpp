ed3eb185f62e085db902d3381d4a48baca7ae0dc
Pale Moon <git-repo@palemoon.org>
2014-11-22 19:59:57 +0100

    ES6: Add Number.MIN_SAFE_INTEGER and Number.MAX_SAFE_INTEGER

--- js/src/jsnum.cpp
+++ js/src/jsnum.cpp
@@ -983,6 +983,8 @@ enum nc_slot {
     NC_NEGATIVE_INFINITY,
     NC_MAX_VALUE,
     NC_MIN_VALUE,
+    NC_MAX_SAFE_INTEGER,
+    NC_MIN_SAFE_INTEGER,
     NC_EPSILON,
     NC_LIMIT
 };
@@ -998,6 +1000,10 @@ static JSConstDoubleSpec number_constants[] = {
     {0,                         "NEGATIVE_INFINITY", 0,{0,0,0}},
     {1.7976931348623157E+308,   "MAX_VALUE",         0,{0,0,0}},
     {0,                         "MIN_VALUE",         0,{0,0,0}},
+    /* ES6 draft 2014-10-14 20.1.2.6 Number.MAX_SAFE_INTEGER constant */
+    {9007199254740991,          "MAX_SAFE_INTEGER",  0,{0,0,0}},
+    /* ES6 draft 2014-10-14 20.1.2.8 Number.MIN_SAFE_INTEGER constant */
+    {-9007199254740991,         "MIN_SAFE_INTEGER",  0,{0,0,0}},
     /* ES6 draft 2014-10-14 20.1.2.1 Number.EPSILON constant */
     {2.2204460492503130808472633361816e-16, "EPSILON", 0,{0,0,0}},
     {0,0,0,{0,0,0}}
