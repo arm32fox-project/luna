f02a02e10b2e8288c112b758d4ceac047f5fe323
Pale Moon <git-repo@palemoon.org>
2014-11-23 22:02:57 +0100

    ES6: Add Map.prototype.forEach and Set.prototype.forEach and required iterators for these functions.
    See ES6 draft 2014-10-14 23.1.3.5 and 23.2.3.6

--- js/src/builtin/MapObject.cpp
+++ js/src/builtin/MapObject.cpp
@@ -1032,6 +1032,7 @@ const JSFunctionSpec MapObject::methods[] = {
     JS_FN("keys", keys, 0, 0),
     JS_FN("values", values, 0, 0),
     JS_FN("clear", clear, 0, 0),
+    {"forEach", {NULL, NULL}, 2, 0, "MapForEach"},
     JS_FS_END
 };
 
@@ -1588,6 +1589,7 @@ const JSFunctionSpec SetObject::methods[] = {
     JS_FN("delete", delete_, 1, 0),
     JS_FN("entries", entries, 0, 0),
     JS_FN("clear", clear, 0, 0),
+    {"forEach", {NULL, NULL}, 2, 0, "SetForEach"},
     JS_FS_END
 };
 
