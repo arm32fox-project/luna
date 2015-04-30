40e3315e7f3fed6937ab41a2f413ddbc1b14e7e3
trav90 <travawine@protonmail.ch>
2015-04-30 03:53:27 -0500

    Remove nsGnomeVFSService

--- toolkit/system/gnome/nsGnomeModule.cpp
+++ toolkit/system/gnome/nsGnomeModule.cpp
@@ -12,10 +12,6 @@
 #include "nsGConfService.h"
 NS_GENERIC_FACTORY_CONSTRUCTOR_INIT(nsGConfService, Init)
 #endif
-#ifdef MOZ_ENABLE_GNOMEVFS
-#include "nsGnomeVFSService.h"
-NS_GENERIC_FACTORY_CONSTRUCTOR_INIT(nsGnomeVFSService, Init)
-#endif
 #ifdef MOZ_ENABLE_GIO
 #include "nsGIOService.h"
 #include "nsGSettingsService.h"
@@ -26,9 +22,6 @@ NS_GENERIC_FACTORY_CONSTRUCTOR_INIT(nsGSettingsService, Init)
 #ifdef MOZ_ENABLE_GCONF
 NS_DEFINE_NAMED_CID(NS_GCONFSERVICE_CID);
 #endif
-#ifdef MOZ_ENABLE_GNOMEVFS
-NS_DEFINE_NAMED_CID(NS_GNOMEVFSSERVICE_CID);
-#endif
 #ifdef MOZ_ENABLE_GIO
 NS_DEFINE_NAMED_CID(NS_GIOSERVICE_CID);
 NS_DEFINE_NAMED_CID(NS_GSETTINGSSERVICE_CID);
@@ -37,9 +30,6 @@ NS_DEFINE_NAMED_CID(NS_GSETTINGSSERVICE_CID);
 static const mozilla::Module::CIDEntry kGnomeCIDs[] = {
 #ifdef MOZ_ENABLE_GCONF
   { &kNS_GCONFSERVICE_CID, false, NULL, nsGConfServiceConstructor },
-#endif
-#ifdef MOZ_ENABLE_GNOMEVFS
-  { &kNS_GNOMEVFSSERVICE_CID, false, NULL, nsGnomeVFSServiceConstructor },
 #endif
 #ifdef MOZ_ENABLE_GIO
   { &kNS_GIOSERVICE_CID, false, NULL, nsGIOServiceConstructor },
@@ -51,9 +41,6 @@ static const mozilla::Module::CIDEntry kGnomeCIDs[] = {
 static const mozilla::Module::ContractIDEntry kGnomeContracts[] = {
 #ifdef MOZ_ENABLE_GCONF
   { NS_GCONFSERVICE_CONTRACTID, &kNS_GCONFSERVICE_CID },
-#endif
-#ifdef MOZ_ENABLE_GNOMEVFS
-  { NS_GNOMEVFSSERVICE_CONTRACTID, &kNS_GNOMEVFSSERVICE_CID },
 #endif
 #ifdef MOZ_ENABLE_GIO
   { NS_GIOSERVICE_CONTRACTID, &kNS_GIOSERVICE_CID },
