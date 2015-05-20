37ec67b7436423a95bbe0f92152e2238455b6711
Pale Moon <git-repo@palemoon.org>
2015-05-20 21:48:35 +0200

    Refuse DHE keys less than 1024 key bits (Logjam)

--- security/nss/lib/ssl/ssl3con.c
+++ security/nss/lib/ssl/ssl3con.c
@@ -6714,7 +6714,7 @@ ssl3_HandleServerKeyExchange(sslSocket *ss, SSL3Opaque *b, PRUint32 length)
     	if (rv != SECSuccess) {
 	    goto loser;		/* malformed. */
 	}
-	if (dh_p.len < 512/8) {
+	if (dh_p.len < 1024/8) {
 	    errCode = SSL_ERROR_WEAK_SERVER_EPHEMERAL_DH_KEY;
 	    goto alert_loser;
 	}
