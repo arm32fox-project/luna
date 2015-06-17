07552cec71779a381ac3fb3266cedc81508d0b55
Pale Moon <git-repo@palemoon.org>
2015-06-17 15:09:02 +0200

    Tweak system headers

--- netwerk/mime/nsMimeTypes.h
+++ netwerk/mime/nsMimeTypes.h
@@ -103,7 +103,7 @@
 #define IMAGE_JNG                           "image/x-jng"
 #define IMAGE_SVG_XML                       "image/svg+xml"
 #define IMAGE_WBMP                          "image/vnd.wap.wbmp"
-#define IMAGE_WEBP			    "image/webp"
+#define IMAGE_WEBP                          "image/webp"
 
 #define MESSAGE_EXTERNAL_BODY               "message/external-body"
 #define MESSAGE_NEWS                        "message/news"
