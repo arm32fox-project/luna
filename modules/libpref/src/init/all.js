4552d2f7fcd9b81e49e782cd9e70bb173212f532
Pale Moon <git-repo@palemoon.org>
2015-07-17 20:13:04 +0200

    Disable network A/B testing

--- modules/libpref/src/init/all.js
+++ modules/libpref/src/init/all.js
@@ -839,7 +839,7 @@ pref("security.fileuri.strict_origin_policy", true);
 // Allow necko to do A/B testing. Will generally only happen if
 // telemetry is also enabled as otherwise there is no way to report
 // the results
-pref("network.allow-experiments", true);
+pref("network.allow-experiments", false);
 
 // Turn off interprocess security checks. Needed to run xpcshell tests.
 pref("network.disable.ipc.security", false);
