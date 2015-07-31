29a610ceaadb9312ada58f66e7d377eb11d369c3
Pale Moon <git-repo@palemoon.org>
2015-07-31 23:38:21 +0200

    Don't expand the rect in unscrollable directions, because bad webdesign.

--- layout/generic/nsGfxScrollFrame.h
+++ layout/generic/nsGfxScrollFrame.h
@@ -404,7 +404,7 @@ protected:
                           nsIAtom *aOrigin, // nullptr indicates "other" origin
                           const nsRect* aRange);
 
-  static nsRect ExpandRect(const nsRect& aRect);
+  nsRect ExpandRect(const nsRect& aRect) const;
   static void EnsureImageVisPrefsCached();
   static bool sImageVisPrefsCached;
   // The number of scrollports horizontally/vertically to expand when looking
