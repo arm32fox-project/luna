3c9c519d6a028967f41f2d811c39e550e1249578
Pale Moon <git-repo@palemoon.org>
2015-09-03 10:52:38 +0200

    Disable battery API by default to reduce private data exposure to the web.

--- modules/libpref/src/init/all.js
+++ modules/libpref/src/init/all.js
@@ -3952,7 +3952,8 @@ pref("dom.vibrator.max_vibrate_ms", 10000);
 pref("dom.vibrator.max_vibrate_list_len", 128);
 
 // Battery API
-pref("dom.battery.enabled", true);
+// Disabled by default to reduce private data exposure
+pref("dom.battery.enabled", false);
 
 // WebSMS
 pref("dom.sms.enabled", false);
