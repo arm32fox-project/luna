c8ebfe491d9d0e63481f727d4fd8d46c3de6f217
trav90 <travawine@protonmail.ch>
2015-12-02 00:49:26 -0600

    Add stdc++compat check for host programs when not cross compiling

--- config/rules.mk
+++ config/rules.mk
@@ -896,6 +896,9 @@ else
 	$(HOST_CC) -o $@ $(HOST_CFLAGS) $(HOST_LDFLAGS) $(HOST_PROGOBJS) $(HOST_LIBS) $(HOST_EXTRA_LIBS)
 endif # HOST_CPP_PROG_LINK
 endif
+ifndef CROSS_COMPILE
+      $(call CHECK_STDCXX,$@)
+endif
 
 #
 # This is an attempt to support generation of multiple binaries
@@ -935,6 +938,9 @@ ifneq (,$(HOST_CPPSRCS)$(USE_HOST_CXX))
 else
 	$(HOST_CC) $(HOST_OUTOPTION)$@ $(HOST_CFLAGS) $(INCLUDES) $< $(HOST_LIBS) $(HOST_EXTRA_LIBS)
 endif
+endif
+ifndef CROSS_COMPILE
+      $(call CHECK_STDCXX,$@)
 endif
 
 ifdef DTRACE_PROBE_OBJ
