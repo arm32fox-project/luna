25975c62af67bd7b469e60078a450ef649e06e09
Pale Moon <git-repo@palemoon.org>
2015-12-25 21:11:48 +0100

    Part 1: Add new eCSSUnit_Unset unit for nsCSSValues.

--- layout/style/nsCSSValue.cpp
+++ layout/style/nsCSSValue.cpp
@@ -403,7 +403,9 @@ void nsCSSValue::SetPairValue(const nsCSSValuePair* aValue)
                     aValue->mXValue.GetUnit() != eCSSUnit_Inherit &&
                     aValue->mYValue.GetUnit() != eCSSUnit_Inherit &&
                     aValue->mXValue.GetUnit() != eCSSUnit_Initial &&
-                    aValue->mYValue.GetUnit() != eCSSUnit_Initial,
+                    aValue->mYValue.GetUnit() != eCSSUnit_Initial &&
+                    aValue->mXValue.GetUnit() != eCSSUnit_Unset &&
+                    aValue->mYValue.GetUnit() != eCSSUnit_Unset,
                     "missing or inappropriate pair value");
   Reset();
   mUnit = eCSSUnit_Pair;
@@ -419,7 +421,9 @@ void nsCSSValue::SetPairValue(const nsCSSValue& xValue,
                     xValue.GetUnit() != eCSSUnit_Inherit &&
                     yValue.GetUnit() != eCSSUnit_Inherit &&
                     xValue.GetUnit() != eCSSUnit_Initial &&
-                    yValue.GetUnit() != eCSSUnit_Initial,
+                    yValue.GetUnit() != eCSSUnit_Initial &&
+                    xValue.GetUnit() != eCSSUnit_Unset &&
+                    yValue.GetUnit() != eCSSUnit_Unset,
                     "inappropriate pair value");
   Reset();
   mUnit = eCSSUnit_Pair;
@@ -439,7 +443,10 @@ void nsCSSValue::SetTripletValue(const nsCSSValueTriplet* aValue)
                       aValue->mZValue.GetUnit() != eCSSUnit_Inherit &&
                       aValue->mXValue.GetUnit() != eCSSUnit_Initial &&
                       aValue->mYValue.GetUnit() != eCSSUnit_Initial &&
-                      aValue->mZValue.GetUnit() != eCSSUnit_Initial,
+                      aValue->mZValue.GetUnit() != eCSSUnit_Initial &&
+                      aValue->mXValue.GetUnit() != eCSSUnit_Unset &&
+                      aValue->mYValue.GetUnit() != eCSSUnit_Unset &&
+                      aValue->mZValue.GetUnit() != eCSSUnit_Unset,
                       "missing or inappropriate triplet value");
     Reset();
     mUnit = eCSSUnit_Triplet;
@@ -459,7 +466,10 @@ void nsCSSValue::SetTripletValue(const nsCSSValue& xValue,
                       zValue.GetUnit() != eCSSUnit_Inherit &&
                       xValue.GetUnit() != eCSSUnit_Initial &&
                       yValue.GetUnit() != eCSSUnit_Initial &&
-                      zValue.GetUnit() != eCSSUnit_Initial,
+                      zValue.GetUnit() != eCSSUnit_Initial &&
+                      xValue.GetUnit() != eCSSUnit_Unset &&
+                      yValue.GetUnit() != eCSSUnit_Unset &&
+                      zValue.GetUnit() != eCSSUnit_Unset,
                       "inappropriate triplet value");
     Reset();
     mUnit = eCSSUnit_Triplet;
@@ -528,6 +538,12 @@ void nsCSSValue::SetInitialValue()
 {
   Reset();
   mUnit = eCSSUnit_Initial;
+}
+
+void nsCSSValue::SetUnsetValue()
+{
+  Reset();
+  mUnit = eCSSUnit_Unset;
 }
 
 void nsCSSValue::SetNoneValue()
@@ -1157,6 +1173,7 @@ nsCSSValue::AppendToString(nsCSSProperty aProperty, nsAString& aResult) const
     case eCSSUnit_Auto:         aResult.AppendLiteral("auto");     break;
     case eCSSUnit_Inherit:      aResult.AppendLiteral("inherit");  break;
     case eCSSUnit_Initial:      aResult.AppendLiteral("initial");  break;
+    case eCSSUnit_Unset:        aResult.AppendLiteral("unset");    break;
     case eCSSUnit_None:         aResult.AppendLiteral("none");     break;
     case eCSSUnit_Normal:       aResult.AppendLiteral("normal");   break;
     case eCSSUnit_System_Font:  aResult.AppendLiteral("-moz-use-system-font"); break;
@@ -1245,6 +1262,7 @@ nsCSSValue::SizeOfExcludingThis(nsMallocSizeOfFun aMallocSizeOf) const
     case eCSSUnit_Auto:
     case eCSSUnit_Inherit:
     case eCSSUnit_Initial:
+    case eCSSUnit_Unset:
     case eCSSUnit_None:
     case eCSSUnit_Normal:
     case eCSSUnit_System_Font:
@@ -1483,7 +1501,8 @@ nsCSSRect::AppendToString(nsCSSProperty aProperty, nsAString& aResult) const
 {
   NS_ABORT_IF_FALSE(mTop.GetUnit() != eCSSUnit_Null &&
                     mTop.GetUnit() != eCSSUnit_Inherit &&
-                    mTop.GetUnit() != eCSSUnit_Initial,
+                    mTop.GetUnit() != eCSSUnit_Initial &&
+                    mTop.GetUnit() != eCSSUnit_Unset,
                     "parser should have used a bare value");
 
   if (eCSSProperty_border_image_slice == aProperty ||
@@ -1634,6 +1653,7 @@ nsCSSValuePairList::AppendToString(nsCSSProperty aProperty,
     item->mXValue.AppendToString(aProperty, aResult);
     if (item->mXValue.GetUnit() != eCSSUnit_Inherit &&
         item->mXValue.GetUnit() != eCSSUnit_Initial &&
+        item->mXValue.GetUnit() != eCSSUnit_Unset &&
         item->mYValue.GetUnit() != eCSSUnit_Null) {
       aResult.Append(PRUnichar(' '));
       item->mYValue.AppendToString(aProperty, aResult);
