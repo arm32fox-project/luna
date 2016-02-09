0117fdd23f74d4bc01b5aeeb6c32cc0b3de9d7d2
Pale Moon <git-repo@palemoon.org>
2016-02-09 12:30:31 +0100

    Adjust cookie gating to allow more exceptions for web compatibility.
    See issue #357

--- netwerk/cookie/nsCookieService.cpp
+++ netwerk/cookie/nsCookieService.cpp
@@ -2791,11 +2791,16 @@ nsCookieService::SetCookieInternal(nsIURI                        *aHostURI,
 
   // Reject cookie if value contains an RFC 6265 disallowed character.
   // See RFC 6265 section 4.1.1
-  // XXX: For now we allow 0x20 (Space) and 0x22 (DQUOTE); the former for web
-  // compatibility and the latter because DQUOTE *cookie-octet DQUOTE is 
-  // permitted and would otherwise fail on the latter. This needs better
-  // checking for first and last character allowed DQUOTE bot not in the actual
-  // value.
+  // XXX: For now we allow for web compatibility (see issue #357):
+  // 0x20 (Space)
+  // 0x22 (DQUOTE)
+  // 0x2C (Comma)
+  // 0x5C (Backslash)
+  //
+  // FIXME: Before removing DQUOTE from the exceptions list:
+  // DQUOTE *cookie-octet DQUOTE is permitted and would fail if just removed.
+  // This needs better checking for first and last character allowing
+  // DQUOTE but not in the actual value.
   //
   // This only applies to cookies set via the Set-Cookie header, since
   // document.cookie is defined to be UTF-8.
@@ -2803,7 +2808,7 @@ nsCookieService::SetCookieInternal(nsIURI                        *aHostURI,
       0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0A, 0x0B,
       0x0C, 0x0D, 0x0E, 0x0F, 0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16,
       0x17, 0x18, 0x19, 0x1A, 0x1B, 0x1C, 0x1D, 0x1E, 0x1F, /* 0x20, 0x22, */
-      0x2C, 0x3B, 0x5C, 0x7F, 0x00 };
+      /* 0x2C, */ 0x3B, /* 0x5C, */ 0x7F, 0x00 };
   if (aFromHttp && (cookieAttributes.value.FindCharInSet(illegalCharacters, 0) != -1)) {
     COOKIE_LOGFAILURE(SET_COOKIE, aHostURI, savedCookieHeader, "illegal character in cookie");
     return newCookie;
