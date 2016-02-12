41a51ef9142d9da69f7718319219f354e0dc8c74
Pale Moon <git-repo@palemoon.org>
2016-02-12 12:15:05 +0100

    Update Graphite2 lib to 1.3.5-e569e28

--- gfx/graphite2/src/Face.cpp
+++ gfx/graphite2/src/Face.cpp
@@ -195,7 +195,6 @@ bool Face::runGraphite(Segment *seg, const Silf *aSilf) const
                 << "output" << json::array;
         for(Slot * s = seg->first(); s; s = s->next())
             *dbgout     << dslot(seg, s);
-        seg->finalise(0);                   // Call this here to fix up charinfo back indexes.
         *dbgout         << json::close
                 << "advance" << seg->advance()
                 << "chars"   << json::array;
@@ -239,7 +238,7 @@ uint16 Face::getGlyphMetric(uint16 gid, uint8 metric) const
         case kgmetAscent : return m_ascent;
         case kgmetDescent : return m_descent;
         default: 
-            if (gid > glyphs().numGlyphs()) return 0;
+            if (gid >= glyphs().numGlyphs()) return 0;
             return glyphs().glyph(gid)->getMetric(metric);
     }
 }
