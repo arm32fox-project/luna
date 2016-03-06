e92f23b9f663d6a87c709cd51088b0a8fdd7f644
Pale Moon <git-repo@palemoon.org>
2016-03-06 16:02:33 +0100

    Update Android sync cipher suites to use GCM on 5+, TLS 1.2 on 4.4+
    Use ECC when possible, otherwise use RSA fallback.
    Also, remove RC4 and SSLv3.

--- mobile/android/base/sync/net/TLSSocketFactory.java
+++ mobile/android/base/sync/net/TLSSocketFactory.java
@@ -15,17 +15,54 @@ import org.mozilla.gecko.background.common.log.Logger;
 import ch.boye.httpclientandroidlib.conn.ssl.SSLSocketFactory;
 import ch.boye.httpclientandroidlib.params.HttpParams;
 
+import android.os.Build; //for version check
+
 public class TLSSocketFactory extends SSLSocketFactory {
+
+  private static final String[] DEFAULT_CIPHER_SUITES;
+  private static final String[] DEFAULT_PROTOCOLS;
+
+  if (Build.VERSION.SDK_INT >= 20) { //Android 5+
+    DEFAULT_CIPHER_SUITES = new String[]
+        {
+         "TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256",
+         "TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256",
+         "TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA",
+         "TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA",
+         
+         // Emergency non-ECC fallbacks
+         "TLS_RSA_WITH_AES_256_CBC_SHA",
+         "TLS_RSA_WITH_AES_128_CBC_SHA",
+         "TLS_RSA_WITH_3DES_EDE_CBC_SHA",
+        };
+  } else {
+    DEFAULT_CIPHER_SUITES = new String[]
+        {
+         "TLS_ECDHE_ECDSA_WITH_AES_256_CBC_SHA",
+         "TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA",
+         "TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA",
+         "TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA",
+         
+         // Emergency non-ECC fallbacks
+         "TLS_RSA_WITH_AES_256_CBC_SHA",
+         "TLS_RSA_WITH_AES_128_CBC_SHA",
+         "TLS_RSA_WITH_3DES_EDE_CBC_SHA",
+        };
+  }
+
+  if (Build.VERSION.SDK_INT >= 19) { //Android 4.4+ supports TLS 1.2
+    DEFAULT_PROTOCOLS = new String[] {
+      "TLSv1.2",
+      "TLSv1.1",
+      "TLSv1",
+    };
+  } else {
+    DEFAULT_PROTOCOLS = new String[] {
+      "TLSv1",
+    };
+  }
+  
   private static final String LOG_TAG = "TLSSocketFactory";
-  private static final String[] DEFAULT_CIPHER_SUITES = new String[] {
-    "TLS_DHE_RSA_WITH_AES_256_CBC_SHA",
-    "TLS_DHE_RSA_WITH_AES_128_CBC_SHA",
-    "SSL_RSA_WITH_RC4_128_SHA", // "RC4_SHA"
-  };
-  private static final String[] DEFAULT_PROTOCOLS = new String[] {
-    "SSLv3",
-    "TLSv1"
-  };
 
   // Guarded by `this`.
   private static String[] cipherSuites = DEFAULT_CIPHER_SUITES;
