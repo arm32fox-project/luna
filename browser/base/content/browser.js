c67f6d5558137a9042613b6a90996a0bbbe77c38
Pale Moon <git-repo@palemoon.org>
2016-05-16 17:53:16 +0200

    Extend window frame color detection to Windows 10, tune the contrast flip point, and correct an error in the value for a missing ColorizationColorBalance key in the registry.

--- browser/base/content/browser.js
+++ browser/base/content/browser.js
@@ -982,7 +982,8 @@ var gBrowserInit = {
     retrieveToolbarIconsizesFromTheme();
 
 #ifdef XP_WIN
-    if (window.matchMedia("(-moz-os-version: windows-win8)").matches &&
+    if ((window.matchMedia("(-moz-os-version: windows-win8)").matches ||
+         window.matchMedia("(-moz-os-version: windows-win10)").matches || true) &&
         window.matchMedia("(-moz-windows-default-theme)").matches) {
       let windows8WindowFrameColor = Cu.import("resource:///modules/Windows8WindowFrameColor.jsm", {}).Windows8WindowFrameColor;
       let windowFrameColor = windows8WindowFrameColor.get();
@@ -1000,7 +1001,7 @@ var gBrowserInit = {
                                 windowFrameColor[2] * 0.0722;
       let foregroundLuminance = 0; // Default to black for foreground text.
       let contrastRatio = (backgroundLuminance + 0.05) / (foregroundLuminance + 0.05);
-      if (contrastRatio < 3) {
+      if (contrastRatio < 4.7) {
         document.documentElement.setAttribute("darkwindowframe", "true");
       }
     }
