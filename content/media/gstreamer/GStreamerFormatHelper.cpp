4433b33073bbb9171987f483b0c31421f31d641e
trav90 <travawine@protonmail.ch>
2016-08-21 09:47:41 -0500

    Use nullptr in GStreamerFormatHelper::CanHandleMediaType

--- content/media/gstreamer/GStreamerFormatHelper.cpp
+++ content/media/gstreamer/GStreamerFormatHelper.cpp
@@ -149,7 +149,7 @@ bool GStreamerFormatHelper::CanHandleMediaType(const nsACString& aMIMEType,
   }
 
   const char *type;
-  NS_CStringGetData(aMIMEType, &type, NULL);
+  NS_CStringGetData(aMIMEType, &type, nullptr);
 
   GstCaps *caps;
   if (aCodecs && !aCodecs->IsEmpty()) {
