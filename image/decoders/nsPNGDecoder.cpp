arm32fox-project/luna

commit 25ca96965c594585ce11ee4c2f08e3ecb51284f9

Be more strict with PNG files with CRC errors and invalid frame sizes if building with APNG support.
This follows the strong recommendation in the spec to reject these frames/files outright.

image/decoders/nsPNGDecoder.cpp
@@ -676,6 +676,15 @@ nsPNGDecoder::info_callback(png_structp png_ptr, png_infop info_ptr)
     }
   }
 
+#ifdef PNG_APNG_SUPPORTED
+  /* Reject any ancillary chunk after IDAT with a bad CRC.
+   * This may cause issues with poorly encoded PNG files with CRC errors
+   * in chunks but implements the strong recommendation for APNGs to
+   * reject files with such errors, following the spec.
+   */
+  png_set_crc_action(png_ptr, PNG_CRC_NO_CHANGE, PNG_CRC_ERROR_QUIT);
+#endif
+
   if (decoder->NeedsNewFrame()) {
     // We know that we need a new frame, so pause input so the decoder
     // infrastructure can give it to us.
@@ -831,6 +840,11 @@ nsPNGDecoder::frame_info_callback(png_structp png_ptr, png_uint_32 frame_num)
   y_offset = png_get_next_frame_y_offset(png_ptr, decoder->mInfo);
   width = png_get_next_frame_width(png_ptr, decoder->mInfo);
   height = png_get_next_frame_height(png_ptr, decoder->mInfo);
+
+  if (width == 0)
+    png_error(png_ptr, "Frame width must not be 0");
+  if (height == 0)
+    png_error(png_ptr, "Frame height must not be 0");
 
   decoder->CreateFrame(x_offset, y_offset, width, height, decoder->format);
 

commit 4e903a60c9491fd91112e17699c55c2dd07b8a22

Fix build bustage caused by 25ca96965c594585ce11ee4c2f08e3ecb51284f9

image/decoders/nsPNGDecoder.cpp
@@ -841,10 +841,14 @@ nsPNGDecoder::frame_info_callback(png_structp png_ptr, png_uint_32 frame_num)
   width = png_get_next_frame_width(png_ptr, decoder->mInfo);
   height = png_get_next_frame_height(png_ptr, decoder->mInfo);
 
-  if (width == 0)
-    png_error(png_ptr, "Frame width must not be 0");
-  if (height == 0)
-    png_error(png_ptr, "Frame height must not be 0");
+  if (width == 0) {
+    PR_LOG(GetPNGLog(), PR_LOG_ERROR, ("libpng error: Frame width must not be 0\n"));
+    png_longjmp(png_ptr, 1);
+  }
+  if (height == 0) {
+    PR_LOG(GetPNGLog(), PR_LOG_ERROR, ("libpng error: Frame height must not be 0\n"));
+    png_longjmp(png_ptr, 1);
+  }
 
   decoder->CreateFrame(x_offset, y_offset, width, height, decoder->format);
 