arm32fox-project/luna

commit 9aab4b803034dbd503fe0477c596c890e6ab83d1

DiD port: Keep strong pointers for nodes in the invalidation list of a document accessible.

accessible/generic/DocAccessible.h
@@ -646,7 +646,7 @@ protected:
    *
    * @see ProcessInvalidationList
    */
-  nsTArray<nsIContent*> mInvalidationList;
+  nsTArray<RefPtr<nsIContent>> mInvalidationList;
 
   /**
    * Used to process notification from core and accessible events.

accessible/generic/DocAccessible.cpp
@@ -116,6 +116,7 @@ NS_IMPL_CYCLE_COLLECTION_TRAVERSE_BEGIN_INHERITED(DocAccessible, Accessible)
   tmp->mDependentIDsHash.EnumerateRead(CycleCollectorTraverseDepIDsEntry, &cb);
   NS_IMPL_CYCLE_COLLECTION_TRAVERSE(mAccessibleCache)
   NS_IMPL_CYCLE_COLLECTION_TRAVERSE(mAnchorJumpElm)
+  NS_IMPL_CYCLE_COLLECTION_TRAVERSE(mInvalidationList)
 NS_IMPL_CYCLE_COLLECTION_TRAVERSE_END
 
 NS_IMPL_CYCLE_COLLECTION_UNLINK_BEGIN_INHERITED(DocAccessible, Accessible)
@@ -126,6 +127,7 @@ NS_IMPL_CYCLE_COLLECTION_UNLINK_BEGIN_INHERITED(DocAccessible, Accessible)
   tmp->mNodeToAccessibleMap.Clear();
   NS_IMPL_CYCLE_COLLECTION_UNLINK(mAccessibleCache)
   NS_IMPL_CYCLE_COLLECTION_UNLINK(mAnchorJumpElm)
+  NS_IMPL_CYCLE_COLLECTION_UNLINK(mInvalidationList)
 NS_IMPL_CYCLE_COLLECTION_UNLINK_END
 
 NS_INTERFACE_MAP_BEGIN_CYCLE_COLLECTION_INHERITED(DocAccessible)

commit eaa21516c7c71e24314fced1e834f5c41f6880b4

Fix build error caused by 9aab4b803034dbd503fe0477c596c890e6ab83d1

accessible/generic/DocAccessible.h
@@ -646,7 +646,7 @@ protected:
    *
    * @see ProcessInvalidationList
    */
-  nsTArray<RefPtr<nsIContent>> mInvalidationList;
+  nsTArray<nsRefPtr<nsIContent>> mInvalidationList;
 
   /**
    * Used to process notification from core and accessible events.