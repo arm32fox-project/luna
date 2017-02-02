arm32fox-project/luna

commit 112e89b2bea522ece7bbdd159e429b531d7e2978

Use getter to access MediaRawData mData and mSize member

dom/media/platforms/apple/AppleATDecoder.cpp
@@ -73,7 +73,7 @@ AppleATDecoder::Input(MediaRawData* aSample)
       aSample->mDuration,
       aSample->mTime,
       aSample->mKeyframe ? " keyframe" : "",
-      (unsigned long long)aSample->mSize);
+      (unsigned long long)aSample->Size());
 
   // Queue a task to perform the actual decoding on a separate thread.
   mTaskQueue->Dispatch(
@@ -222,7 +222,7 @@ AppleATDecoder::DecodeSample(MediaRawData* aSample)
   // This API insists on having packets spoon-fed to it from a callback.
   // This structure exists only to pass our state.
   PassthroughUserData userData =
-    { channels, (UInt32)aSample->mSize, aSample->mData };
+    { channels, (UInt32)aSample->Size(), aSample->Data() };
 
   // Decompressed audio buffer
   nsAutoArrayPtr<AudioDataValue> decoded(new AudioDataValue[maxDecodedSamples]);
@@ -494,8 +494,8 @@ AppleATDecoder::GetImplicitAACMagicCookie(const MediaRawData* aSample)
   }
 
   OSStatus status = AudioFileStreamParseBytes(mStream,
-                                              adtssample->mSize,
-                                              adtssample->mData,
+                                              adtssample->Size(),
+                                              adtssample->Size(),
                                               0 /* discontinuity */);
   if (status) {
     NS_WARNING("Couldn't parse sample");

dom/media/MediaData.cpp
@@ -486,9 +486,9 @@ VideoData::Create(const VideoInfo& aInfo,
 
 MediaRawData::MediaRawData()
   : MediaData(RAW_DATA)
+  , mCrypto(mCryptoInternal)
   , mData(nullptr)
   , mSize(0)
-  , mCrypto(mCryptoInternal)
   , mBuffer(new MediaLargeByteBuffer(RAW_DATA_DEFAULT_SIZE))
   , mPadding(0)
 {
@@ -496,9 +496,9 @@ MediaRawData::MediaRawData()
 
 MediaRawData::MediaRawData(const uint8_t* aData, size_t aSize)
   : MediaData(RAW_DATA)
+  , mCrypto(mCryptoInternal)
   , mData(nullptr)
   , mSize(0)
-  , mCrypto(mCryptoInternal)
   , mBuffer(new MediaLargeByteBuffer(RAW_DATA_DEFAULT_SIZE))
   , mPadding(0)
 {
@@ -585,28 +585,21 @@ MediaRawData::CreateWriter()
 }
 
 MediaRawDataWriter::MediaRawDataWriter(MediaRawData* aMediaRawData)
-  : mData(nullptr)
-  , mSize(0)
-  , mCrypto(aMediaRawData->mCryptoInternal)
+  : mCrypto(aMediaRawData->mCryptoInternal)
   , mTarget(aMediaRawData)
   , mBuffer(aMediaRawData->mBuffer.get())
 {
-  if (aMediaRawData->mData) {
-    mData = mBuffer->Elements() + mTarget->mPadding;
-    mSize = mTarget->mSize;
-  }
 }
 
 bool
 MediaRawDataWriter::EnsureSize(size_t aSize)
 {
-  if (aSize <= mSize) {
+  if (aSize <= mTarget->mSize) {
     return true;
   }
   if (!mTarget->EnsureCapacity(aSize)) {
     return false;
   }
-  mData = mBuffer->Elements() + mTarget->mPadding;
   return true;
 }
 
@@ -618,7 +611,7 @@ MediaRawDataWriter::SetSize(size_t aSize)
   }
   // Pad our buffer.
   mBuffer->SetLength(aSize + mTarget->mPadding + RAW_DATA_ALIGNMENT);
-  mTarget->mSize = mSize = aSize;
+  mTarget->mSize = aSize;
   return true;
 }
 
@@ -630,7 +623,6 @@ MediaRawDataWriter::Prepend(const uint8_t* aData, size_t aSize)
   }
   mBuffer->InsertElementsAt(mTarget->mPadding, aData, aSize);
   mTarget->mSize += aSize;
-  mSize = mTarget->mSize;
   return true;
 }
 
@@ -641,7 +633,7 @@ MediaRawDataWriter::Replace(const uint8_t* aData, size_t aSize)
     return false;
   }
   mBuffer->ReplaceElementsAt(mTarget->mPadding, mTarget->mSize, aData, aSize);
-  mTarget->mSize = mSize = aSize;
+  mTarget->mSize = aSize;
   return true;
 }
 
@@ -649,8 +641,20 @@ void
 MediaRawDataWriter::Clear()
 {
   mBuffer->RemoveElementsAt(mTarget->mPadding, mTarget->mSize);
-  mTarget->mSize = mSize = 0;
-  mTarget->mData = mData = nullptr;
+  mTarget->mSize = 0;
+  mTarget->mData = nullptr;
+}
+
+uint8_t*
+MediaRawDataWriter::Data()
+{
+  return mTarget->mData;
+}
+
+size_t
+MediaRawDataWriter::Size()
+{
+  return mTarget->Size();
 }
 
 } // namespace mozilla

dom/media/MediaData.h
@@ -345,9 +345,9 @@ class MediaRawDataWriter
 {
 public:
   // Pointer to data or null if not-yet allocated
-  uint8_t* mData;
+  uint8_t* Data();
   // Writeable size of buffer.
-  size_t mSize;
+  size_t Size();
   // Writeable reference to MediaRawData::mCryptoInternal
   CryptoSample& mCrypto;
 
@@ -360,7 +360,7 @@ public:
   bool Prepend(const uint8_t* aData, size_t aSize);
   // Replace current content with aData.
   bool Replace(const uint8_t* aData, size_t aSize);
-  // Clear the memory buffer. Will set mData and mSize to 0.
+  // Clear the memory buffer. Will set target mData and mSize to 0.
   void Clear();
 
 private:
@@ -377,9 +377,15 @@ public:
   MediaRawData(const uint8_t* aData, size_t mSize);
 
   // Pointer to data or null if not-yet allocated
-  const uint8_t* mData;
+  const uint8_t* Data() const
+  {
+    return mData;
+  }
   // Size of buffer.
-  size_t mSize;
+  size_t Size() const
+  {
+    return mSize;
+  }
 
   const CryptoSample& mCrypto;
   nsRefPtr<MediaByteBuffer> mExtraData;
@@ -402,6 +408,8 @@ private:
   // read as required by some data decoders.
   // Returns false if memory couldn't be allocated.
   bool EnsureCapacity(size_t aSize);
+  uint8_t* mData;
+  size_t mSize;
   nsRefPtr<MediaLargeByteBuffer> mBuffer;
   CryptoSample mCryptoInternal;
   uint32_t mPadding;

dom/media/gmp/GMPAudioHost.cpp
@@ -40,7 +40,7 @@ GMPAudioSamplesImpl::GMPAudioSamplesImpl(MediaRawData* aSample,
  , mChannels(aChannels)
  , mRate(aRate)
 {
-  mBuffer.AppendElements(aSample->mData, aSample->mSize);
+  mBuffer.AppendElements(aSample->Data(), aSample->Size());
   if (aSample->mCrypto.valid) {
     mCrypto = new GMPEncryptedBufferDataImpl(aSample->mCrypto);
   }

dom/media/platforms/agnostic/gmp/GMPVideoDecoder.cpp
@@ -126,13 +126,13 @@ GMPVideoDecoder::CreateFrame(MediaRawData* aSample)
   }
 
   GMPUnique<GMPVideoEncodedFrame>::Ptr frame(static_cast<GMPVideoEncodedFrame*>(ftmp));
-  err = frame->CreateEmptyFrame(aSample->mSize);
+  err = frame->CreateEmptyFrame(aSample->Size());
   if (GMP_FAILED(err)) {
     mCallback->Error();
     return nullptr;
   }
 
-  memcpy(frame->Buffer(), aSample->mData, frame->Size());
+  memcpy(frame->Buffer(), aSample->Data(), frame->Size());
 
   // Convert 4-byte NAL unit lengths to host-endian 4-byte buffer lengths to
   // suit the GMP API.

dom/media/platforms/android/AndroidDecoderModule.cpp
@@ -464,7 +464,7 @@ void MediaCodecDataDecoder::DecoderLoop()
 
         void* directBuffer = frame.GetEnv()->GetDirectBufferAddress(buffer.Get());
 
-        MOZ_ASSERT(frame.GetEnv()->GetDirectBufferCapacity(buffer.Get()) >= sample->mSize,
+        MOZ_ASSERT(frame.GetEnv()->GetDirectBufferCapacity(buffer.Get()) >= sample->Size(),
           "Decoder buffer is not large enough for sample");
 
         {
@@ -473,9 +473,9 @@ void MediaCodecDataDecoder::DecoderLoop()
           mQueue.pop();
         }
 
-        PodCopy((uint8_t*)directBuffer, sample->mData, sample->mSize);
+        PodCopy((uint8_t*)directBuffer, sample->Data(), sample->Size());
 
-        res = mDecoder->QueueInputBuffer(inputIndex, 0, sample->mSize,
+        res = mDecoder->QueueInputBuffer(inputIndex, 0, sample->Size(),
                                          sample->mTime, 0);
         HANDLE_DECODER_ERROR();
 

dom/media/platforms/apple/AppleVDADecoder.cpp
@@ -104,7 +104,7 @@ AppleVDADecoder::Input(MediaRawData* aSample)
       aSample->mTime,
       aSample->mDuration,
       aSample->mKeyframe ? " keyframe" : "",
-      aSample->mSize);
+      aSample->Size());
 
   mTaskQueue->Dispatch(
       NS_NewRunnableMethodWithArg<nsRefPtr<MediaRawData>>(
@@ -302,7 +302,7 @@ nsresult
 AppleVDADecoder::SubmitFrame(MediaRawData* aSample)
 {
   AutoCFRelease<CFDataRef> block =
-    CFDataCreate(kCFAllocatorDefault, aSample->mData, aSample->mSize);
+    CFDataCreate(kCFAllocatorDefault, aSample->Data(), aSample->Size());
   if (!block) {
     NS_ERROR("Couldn't create CFData");
     return NS_ERROR_FAILURE;

dom/media/platforms/apple/AppleVTDecoder.cpp
@@ -86,7 +86,7 @@ AppleVTDecoder::Input(MediaRawData* aSample)
       aSample->mTime,
       aSample->mDuration,
       aSample->mKeyframe ? " keyframe" : "",
-      aSample->mSize);
+      aSample->Size());
 
 #ifdef LOG_MEDIA_SHA1
   SHA1Sum hash;
@@ -216,12 +216,12 @@ AppleVTDecoder::SubmitFrame(MediaRawData* aSample)
   // But note that there may be a problem keeping the samples
   // alive over multiple frames.
   rv = CMBlockBufferCreateWithMemoryBlock(kCFAllocatorDefault, // Struct allocator.
-                                          const_cast<uint8_t*>(aSample->mData),
-                                          aSample->mSize,
+                                          const_cast<uint8_t*>(aSample->Data()),
+                                          aSample->Size(),
                                           kCFAllocatorNull, // Block allocator.
                                           NULL, // Block source.
                                           0,    // Data offset.
-                                          aSample->mSize,
+                                          aSample->Size(),
                                           false,
                                           block.receive());
   if (rv != noErr) {

commit 5b6e0ff055765b430806e22a27c55abc17368aad

Fix a build failed on macOS
caused by commit 112e89b2bea522ece7bbdd159e429b531d7e2978

dom/media/platforms/apple/AppleATDecoder.cpp
@@ -495,7 +495,7 @@ AppleATDecoder::GetImplicitAACMagicCookie(const MediaRawData* aSample)
 
   OSStatus status = AudioFileStreamParseBytes(mStream,
                                               adtssample->Size(),
-                                              adtssample->Size(),
+                                              adtssample->Data(),
                                               0 /* discontinuity */);
   if (status) {
     NS_WARNING("Couldn't parse sample");