f2bc0785b9852dae1b6f5c700fbca41e573aa916
wolfbeast <mcwerewolf@gmail.com>
2018-04-13 22:06:20 +0200

    Convert MOZILLA_OFFICIAL conditionals to MC_OFFICIAL where needed.
    - `--enable-official-branding` implies `MC_OFFICIAL` (no need to specifically set it)
    - `--enable-official-vendor` can be used to set `MC_OFFICIAL` on builds without `--enable-official-branding` that should still be considered official release versions.
    - `MC_OFFICIAL` implies `--enable-release`, meaning `DEVELOPER_OPTIONS` isn't set
    - `MC_OFFICIAL` makes `nsXULAppInfo.getIsOfficial` return `true`
    - `MC_OFFICIAL` makes `AppConstants.MOZILLA_OFFICIAL` (for compatibility in extensions) and `AppConstants.MC_OFFICIAL` return `true`
    - Optional, for the time being: `MOZILLA_OFFICIAL` is still present in some places in case someone wants to build a Mozilla-alike official application and has the rights and necessary keys to use Mozilla-official third-party services. This must always be combined with `MC_OFFICIAL` to have a sane combination of defines. This may be removed in the future.

--- toolkit/xre/nsAppRunner.cpp
+++ toolkit/xre/nsAppRunner.cpp
@@ -1023,7 +1023,7 @@ nsXULAppInfo::GetDistributionID(nsACString& aResult)
 NS_IMETHODIMP
 nsXULAppInfo::GetIsOfficial(bool* aResult)
 {
-#ifdef MOZILLA_OFFICIAL
+#ifdef MC_OFFICIAL
   *aResult = true;
 #else
   *aResult = false;
