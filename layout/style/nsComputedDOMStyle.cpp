4dae04e594f6662dcfb03fd304494841ca542091
athenian200 <athenian200@outlook.com>
2020-10-23 14:34:55 -0500

    Issue #1673 - Part 1: Allow tab-size to accept <length>.
    Currently -moz-tab-size only accepts <number> values, and both Chrome and Firefox currently support <length> values and have for some time now. So with this you would be able to support sizes in px or em, for instance. This was implemented in Firefox 53 and was trivial to backport.

--- layout/style/nsComputedDOMStyle.cpp
+++ layout/style/nsComputedDOMStyle.cpp
@@ -3972,7 +3972,7 @@ already_AddRefed<CSSValue>
 nsComputedDOMStyle::DoGetTabSize()
 {
   RefPtr<nsROCSSPrimitiveValue> val = new nsROCSSPrimitiveValue;
-  val->SetNumber(StyleText()->mTabSize);
+  SetValueToCoord(val, StyleText()->mTabSize, true);
   return val.forget();
 }
 
