684000d9a5be65aaea5864ae1869d7cab737e089
Eitan Isaacson <eitan@monotonous.org>
2021-07-14 16:40:33 +0000

    [accessibility] Remove selection listeners from shutting down PresShell.

--- accessible/base/SelectionManager.cpp
+++ accessible/base/SelectionManager.cpp
@@ -120,6 +120,24 @@ SelectionManager::RemoveDocSelectionListener(nsIPresShell* aPresShell)
   // selection.
   Selection* spellSel = frameSel->GetSelection(SelectionType::eSpellCheck);
   spellSel->RemoveSelectionListener(this);
+
+  if (mCurrCtrlNormalSel) {
+    if (mCurrCtrlNormalSel->GetPresShell() == aPresShell) {
+      // Remove 'this' registered as selection listener for the normal selection
+      // if we are removing listeners for its PresShell.
+      mCurrCtrlNormalSel->RemoveSelectionListener(this);
+      mCurrCtrlNormalSel = nullptr;
+    }
+  }
+
+  if (mCurrCtrlSpellSel) {
+    if (mCurrCtrlSpellSel->GetPresShell() == aPresShell) {
+      // Remove 'this' registered as selection listener for the spellcheck
+      // selection if we are removing listeners for its PresShell.
+      mCurrCtrlSpellSel->RemoveSelectionListener(this);
+      mCurrCtrlSpellSel = nullptr;
+    }
+  }
 }
 
 void
