63b8aaa49509ef54628061db640bc0afdf0b5a1a
Moonchild <moonchild@palemoon.org>
2021-11-29 17:19:16 -0500

    Issue #1845 - Remove Marionette files

--- testing/mach_commands.py
+++ testing/mach_commands.py
@@ -56,16 +56,6 @@ TEST_SUITES = {
         'mach_command': 'crashtest',
         'kwargs': {'test_file': None},
     },
-    'firefox-ui-functional': {
-        'aliases': ('Fxfn',),
-        'mach_command': 'firefox-ui-functional',
-        'kwargs': {},
-    },
-    'firefox-ui-update': {
-        'aliases': ('Fxup',),
-        'mach_command': 'firefox-ui-update',
-        'kwargs': {},
-    },
     'jetpack': {
         'aliases': ('J',),
         'mach_command': 'jetpack-test',
@@ -138,18 +128,6 @@ TEST_FLAVORS = {
     'chrome': {
         'mach_command': 'mochitest',
         'kwargs': {'flavor': 'chrome', 'test_paths': []},
-    },
-    'firefox-ui-functional': {
-        'mach_command': 'firefox-ui-functional',
-        'kwargs': {'tests': []},
-    },
-    'firefox-ui-update': {
-        'mach_command': 'firefox-ui-update',
-        'kwargs': {'tests': []},
-    },
-    'marionette': {
-        'mach_command': 'marionette-test',
-        'kwargs': {'tests': []},
     },
     'mochitest': {
         'mach_command': 'mochitest',
